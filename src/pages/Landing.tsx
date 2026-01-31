bfa120d76d3286a795c2f527b39665831f9654eb
Roqeeb <shafiriyuroqeeb@gmail.com>
2026-01-31 01:30:17 +0100

    added bg image

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,22 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Programs from "../components/Programs";
+import bgImage from "../assets/shridhar-thorat-6AKLKt-KmdY-unsplash.jpg";
 
 export default function Landing() {
   return (
-    <main className="bg-blue-700">
-      <Navbar />
-      <Hero />
-      <Programs />
+    <main
+      className="relative bg-cover bg-center min-h-screen"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="absolute inset-0 bg-black/20"></div>
+
+      {/* Content */}
+      <div className="relative z-10">
+        <Navbar />
+        <Hero />
+        <Programs />
+      </div>
     </main>
   );
 }
